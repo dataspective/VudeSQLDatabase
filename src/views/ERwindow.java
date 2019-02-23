4ed564af06d5c4070e71cf37f92f107d48dd8a37
Derek Fermaint <fermaint.derek@gmail.com>
2019-02-23 14:18:58 -0800

    Initial commit

---
+++ src/views/ERwindow.java
@@ -0,0 +1,123 @@
+package views;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import common.SQLFetcher;
+
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+
+
+public class ERwindow extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JButton btnRun;
+	private JTextArea displayResults;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ERwindow frame = new ERwindow();
+
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	// Create the frame.
+	public ERwindow() {
+
+		initComponent();
+		createEvents();
+
+	}
+
+	// code for initializing events
+	private void initComponent() {
+
+		setTitle("ER Querie Displayer");
+		setIconImage(
+				Toolkit.getDefaultToolkit().getImage(ERwindow.class.getResource("/resources/atom.png")));
+		setFont(new Font("Dialog", Font.BOLD, 12));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 559, 228);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		displayResults = new JTextArea();
+		displayResults.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.add(displayResults);
+		
+		setContentPane(contentPane);
+
+		btnRun = new JButton("Run");
+
+		textField = new JTextField();
+		textField.setColumns(10);
+
+		JLabel lblEnterYourQuerie = new JLabel("Enter your querie : ");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING).addGroup(gl_contentPane
+				.createSequentialGroup()
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup().addContainerGap().addComponent(btnRun,
+								GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+								.addComponent(lblEnterYourQuerie, GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(textField, GroupLayout.PREFERRED_SIZE, 400, GroupLayout.PREFERRED_SIZE)))
+				.addGap(90)));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
+				.createSequentialGroup().addGap(50)
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblEnterYourQuerie, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE))
+				.addGap(18).addComponent(btnRun, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+				.addContainerGap(46, Short.MAX_VALUE)));
+		contentPane.setLayout(gl_contentPane);
+	}
+
+	// code for creating events
+	private void createEvents() {
+		try {
+
+			btnRun.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					SQLFetcher.query(textField);
+					//JOptionPane.showMessageDialog(null, SQLFetcher.query(textField));
+				}
+
+			});
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, " Invalid input");
+			System.exit(1);
+		}
+
+	}
+}
